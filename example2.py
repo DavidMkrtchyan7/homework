14b515bde597151cd6648d1a676a83aad2a4cebb
DavidMkrtchyan7 <davidmkrtchyan7@gmail.com>
2018-10-01 00:13:11 +0500

    example2

---
+++ example2.py
@@ -0,0 +1,12 @@
+﻿username = "Batman"
+password = "I am Batman"
+input_username = input("username:")
+input_password = input("password:")
+
+for i in range(500000000000000):
+	if input_password == password and input_username == username:
+		print("Welcome")
+		break
+	else:
+		input_username = input("username:")
+		input_password = input("password:") 
