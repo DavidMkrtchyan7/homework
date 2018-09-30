a4213752110ba60fc06d87e82004988ccae8b3f5
DavidMkrtchyan7 <davidmkrtchyan7@gmail.com>
2018-10-01 00:14:35 +0500

    example3

---
+++ example3.py
@@ -0,0 +1,10 @@
+﻿a=[[0]*4]*4
+for elem in a: 
+    print(elem)
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        if i == j:
+            a[i][j] = 1
+        else:
+            a[i][j] = 2
+    print(a[i])       	
